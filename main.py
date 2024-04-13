ed1d8fa474fba889c75f364ccffd99c7166db802
jvasquezt2004 <jorgealonsovasquez@hotmail.com>
2024-04-12 19:29:39 -0600

    Agregadas las operaciones de gamma, inversa y ecualizacion

--- main.py
+++ main.py
@@ -28,7 +28,21 @@ def plot_color_distributions(frequencies):
     plt.show()
 
 
-def apply_operation(image_array, operation, value):
+def make_equalization(image_array):
+    output_image = np.zeros_like(image_array)
+    for channel in range(3):
+        channel_data = image_array[:, :, channel]
+        histogram = np.zeros(256, dtype=int)
+        for i in range(256):
+            histogram[i] = np.sum(channel_data == i)
+        cdf = histogram.cumsum()
+        cdf_normalized = cdf * 255 / cdf[-1]
+        equalized_data = cdf_normalized[channel_data.flatten()].astype(np.uint8)
+        output_image[:, :, channel] = equalized_data.reshape(channel_data.shape)
+    return output_image
+
+
+def apply_operation(image_array, operation, value=1.0):
     if operation == "sumar":
         modified_array = np.clip(image_array + value, 0, 255)
     elif operation == "restar":
@@ -37,6 +51,13 @@ def apply_operation(image_array, operation, value):
         modified_array = np.clip(image_array * value, 0, 255)
     elif operation == "dividir":
         modified_array = np.clip(image_array / value, 0, 255)
+    elif operation == "gamma":
+        gamma_corrected = np.array(255 * (image_array / 255) ** value, dtype="uint8")
+        modified_array = gamma_corrected
+    elif operation == "inversa":
+        modified_array = 255 - image_array
+    elif operation == "ecualizacion":
+        modified_array = make_equalization(image_array)
     else:
         return image_array
     return modified_array.astype(np.uint8)
@@ -45,9 +66,11 @@ def apply_operation(image_array, operation, value):
 def main():
     picture = Image.open("img/image_clear.jpeg")
     operation = input(
-        "Que operacion desea realizar? (sunmar, restar, multiplicar, dividir): "
+        "Que operacion desea realizar? (sunmar, restar, multiplicar, dividir, gamma, inversa, ecualizacion): "
     )
-    value = float(input("Ingrese el valor para la operacion: "))
+    value = 1.0
+    if operation in ["sunmar", "restar", "multiplicar", "dividir", "gamma", "inversa"]:
+        value = float(input("Ingrese el valor para la operacion: "))
 
     picture_array = np.array(picture.convert("RGB"))
     modified_array = apply_operation(picture_array, operation, value)
